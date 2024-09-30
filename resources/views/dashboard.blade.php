473d7d6ca985e00505b125957a469f2f37f16ea2
ShaerWare <artemsher@gmail.com>
2024-10-01 00:21:36 +0500

    diz and breez

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="main-content app-content">
+            <div class="container-fluid">
+                <!-- Page Header -->
+                <div class="d-md-flex d-block align-items-center justify-content-between my-4 page-header-breadcrumb">
+                    <div>
+                        <h4 class="mb-0">Hi, welcome back!</h4>
+                        <p class="mb-0 text-muted">Sales monitoring dashboard template.</p>
+                    </div>
+
+        </h2></div></div></div>
+    </x-slot>
+
+
+</x-app-layout>
